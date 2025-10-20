43447366c43b42245f2b0433586855950ca863f5
Nathan Burchell <u1734837@uel.ac.uk>
2025-10-20 17:46:08 +0100

    Update config

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>NB news feed app</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Weeeeee a button {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Waaaaaah I shall become famous with the NB app
       </p>
     </>
   )
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/nb-news-feed-app/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
   },
-});
+}))
