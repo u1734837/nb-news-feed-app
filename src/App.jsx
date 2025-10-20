190ee805c82530c1606de2ea90952c041eb342b6
Nathan Burchell <u1734837@uel.ac.uk>
2025-10-20 19:14:41 +0100

    Apple icon

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>NB news feed app</h1>
+      <h1>NB news feed app2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Weeeeee a button {count}
